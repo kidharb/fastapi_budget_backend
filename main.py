e6d694c43943f1baec4f95c3d666a53cb9993ff0
Kidhar Bachan <kidharb@gmail.com>
2024-12-04 20:58:47 +0200

    API up n running

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -10,3 +11,5 @@ async def read_root():
 async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
